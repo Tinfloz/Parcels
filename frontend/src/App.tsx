2218a47be3cb23db2059daf7dc815fd7f3d9396d
Tinfloz <swapnonil0007@gmail.com>
2022-12-30 23:33:35 -0500

    init CRA

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  theme,
+} from "@chakra-ui/react"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Text>Hello</Text>
+    </Box>
+  </ChakraProvider>
+)
